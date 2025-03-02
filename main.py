303229864e82f9dc64c45bd67a04a68b86230b36
RipXUzoth <akkuharish1@gmail.com>
2025-03-02 18:52:53 +0530

    Bday vars

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # creating different variables
-x = 5
-y = "John"
+x = 5/8/1988
+y = 4/6/2006
+z = 26/3/2013
+a = 23/5/1972
+b = 21/3/2010
 print (x)
 print(y)
-codingal = "codingal"
-print (codingal)
+print(z)
+print(a)
+print(b)
+birthday = "your birthday"
+print (birthday)
 #user input
-name = input("enter your name: ")
-print("\nHello", name,  "\nwelcome to codingal")
+name = input("press enter to see all birthdays stored: ")
+print("\nHello", x, y, z, a, b  "\nthese are all the birthdays stored")
