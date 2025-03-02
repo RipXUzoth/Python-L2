dcd91921dc576f23ccb6233d2a77139cac5d54ef
RipXUzoth <akkuharish1@gmail.com>
2025-03-02 11:48:26 +0530

    Welcome to codingal

---
+++ main.py
@@ -0,0 +1,10 @@
+# creating different variables
+x = 5
+y = "John"
+print (x)
+print(y)
+codingal = "codingal"
+print (codingal)
+#user input
+name = input("enter your name: ")
+print("\nHello", name,  "\nwelcome to codingal")
